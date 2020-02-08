848d8659df8087e24baac5f0fcce5073f0a4cb24
Lim Zhe Shiong <>
2020-02-08 13:15:59 +0800

    initial project commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+	@Bean
+	   public RestTemplate getRestTemplate() {
+	      return new RestTemplate();
+	   }
+
+}
---
+++ src/main/java/com/example/demo/controller/ProductServiceController.java
@@ -0,0 +1,46 @@
+package com.example.demo.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Product;
+import com.example.demo.service.ProductService;
+
+@RestController
+public class ProductServiceController {
+	@Autowired
+	ProductService productService;
+
+
+	@RequestMapping(value = "/products/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Object> delete(@PathVariable("id") String id) {
+		productService.deleteProduct(id);
+		return new ResponseEntity<>("Product is deleted successsfully", HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/products/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Object> updateProduct(@PathVariable("id") String id, @RequestBody Product product) {
+		productService.updateProduct(id, product);
+		return new ResponseEntity<>("Product is updated successsfully", HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/products", method = RequestMethod.POST)
+	public ResponseEntity<Object> createProduct(@RequestBody Product product) {
+		productService.createProduct(product);
+		return new ResponseEntity<>("Product is created successfully", HttpStatus.CREATED);
+	}
+
+	@RequestMapping(value = "/products")
+	public ResponseEntity<Object> getProduct() {
+		return new ResponseEntity<>(productService.getProducts(), HttpStatus.OK);
+	}
+}
